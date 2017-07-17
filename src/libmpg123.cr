47675ce4180b26eca46cf166fd9dfd8ea5637b2f
mjago <martyn.jago@btinternet.com>
2017-07-17 20:17:53 +0100

    decode() method

--- src/libmpg123.cr
+++ src/libmpg123.cr
@@ -4,155 +4,159 @@ require "./libmpg123/*"
 
 @[Link("mpg123")]
 lib LibMP
-alias MPG_Export = Void
-alias CInt = LibC::Int
-alias CLong = LibC::Long
-alias CFloat = LibC::Float
-alias CDouble = LibC::Double
-alias CChar = LibC::Char
-alias CUChar = LibC::UChar
-alias MP_Handle = LibC::Int
-alias Size = CLong
-alias Offset = CInt
-fun plain_error_str = mpg123_plain_strerror(code : CInt) : CChar*
-fun error_str = mpg123_strerror(hndl : MP_Handle) : CChar*
-fun init = mpg123_init() : CInt
-fun exit = mpg123_exit() : CInt
-fun decoders = mpg123_decoders() : CChar**
-fun supported_decoders = mpg123_supported_decoders() : CChar**
-fun current_decoder = mpg123_current_decoder(handle : MP_Handle*) : CChar*
-fun new = mpg123_new(decoder : CChar*, err : CInt*) : MP_Handle*
-fun open = mpg123_open(MP_Handle*, path : CChar*) : CInt
-fun read = mpg123_read(MP_Handle*, outmemory : CChar*, outmemsize : Size, done : Size*) : CInt
-fun encsize =  mpg123_encsize(encoding : CInt) : CInt
-fun feed = mpg123_feed(MP_Handle*, CUChar*, Size)
-fun delete = mpg123_delete(mh : MP_Handle*) : Void
-fun param = mpg123_param(mh : MP_Handle*, type : MP_Parms, value : CLong, fvalue : CDouble) : Void
-fun buf_size = mpg123_outblock(MP_Handle*) : Size
-fun get_format = mpg123_getformat(MP_Handle*, rate : CLong*, channels : CInt*, encoding : CInt*) : CInt
-fun decode_frame = mpg123_decode_frame(MP_Handle*, Offset* , CUChar** , Size* );
-#int mpg123_decode_frame(mpg123_handle *mh, off_t* num, unsigned char** audio, size_t* 	bytes)
-enum MP_Parms
-MP_VERBOSE = 0
-MP_FLAGS
-MP_ADD_FLAGS MP_FORCE_RATE
-MP_DOWN_SAMPLE MP_RVA
-MP_DOWNSPEED MP_UPSPEED
-MP_START_FRAME MP_DECODE_FRAMES
-MP_ICY_INTERVAL MP_OUTSCALE
-MP_TIMEOUT MP_REMOVE_FLAGS
-MP_RESYNC_LIMIT MP_INDEX_SIZE
-MP_PREFRAMES MP_FEEDPOOL
-MP_FEEDBUFFER
-end
+  alias MPG_Export = Void
+  alias CInt = LibC::Int
+  alias CLong = LibC::Long
+  alias CFloat = LibC::Float
+  alias CDouble = LibC::Double
+  alias CChar = LibC::Char
+  alias CUChar = LibC::UChar
+  alias MP_Handle = LibC::Int
+  alias Size = CLong
+  alias Offset = CInt
 
-enum MP_ParamFlags
-MP_FORCE_MONO = 0x7
-MP_MONO_LEFT = 0x1
-MP_MONO_RIGHT = 0x2
-MP_MONO_MIX = 0x4
-MP_FORCE_STEREO = 0x8
-MP_FORCE_8BIT = 0x10
-MP_QUIET = 0x20
-MP_GAPLESS = 0x40
-MP_NO_RESYNC = 0x80
-MP_SEEKBUFFER = 0x100
-MP_FUZZY = 0x200
-MP_FORCE_FLOAT = 0x400
-MP_PLAIN_ID3TEXT = 0x800
-MP_IGNORE_STREAMLENGTH = 0x1000
-MP_SKIP_ID3V2 = 0x2000
-MP_IGNORE_INFOFRAME = 0x4000
-MP_AUTO_RESAMPLE = 0x8000
-MP_PICTURE = 0x10000
-MP_NO_PEEK_END = 0x20000
-MP_FORCE_SEEKABLE = 0x40000
-end
+  fun plain_error_str = mpg123_plain_strerror(code : CInt) : CChar*
+  fun error_str = mpg123_strerror(hndl : MP_Handle) : CChar*
+  fun init = mpg123_init : CInt
+  fun exit = mpg123_exit : CInt
+  fun decoders = mpg123_decoders : CChar**
+  fun supported_decoders = mpg123_supported_decoders : CChar**
+  fun current_decoder = mpg123_current_decoder(handle : MP_Handle*) : CChar*
+  fun new = mpg123_new(decoder : CChar*, err : CInt*) : MP_Handle*
+  fun open = mpg123_open(MP_Handle*, path : CChar*) : CInt
+  fun read = mpg123_read(MP_Handle*, outmemory : CChar*, outmemsize : Size, done : Size*) : CInt
+  fun encsize = mpg123_encsize(encoding : CInt) : CInt
+  fun feed = mpg123_feed(MP_Handle*, CUChar*, Size) : CInt
+  fun open_feed = mpg123_open_feed(MP_Handle*) : CInt
+  fun delete = mpg123_delete(mh : MP_Handle*) : Void
+  fun param = mpg123_param(mh : MP_Handle*, type : MP_Parms, value : CLong, fvalue : CDouble) : Void
+  fun buf_size = mpg123_outblock(MP_Handle*) : Size
+  fun get_format = mpg123_getformat(MP_Handle*, rate : CLong*, channels : CInt*, encoding : CInt*) : CInt
+  fun decode_frame = mpg123_decode_frame(MP_Handle*, Offset*, CUChar**, Size*) : CInt
+  fun decode = mpg123_decode(MP_Handle*, CUChar*, Size, CUChar*, Size, Size*) : CInt
+  fun meta_free = mpg123_meta_free(MP_Handle*) : CInt
 
-enum MP_Param_RVA
-MP_RVA_OFF = 0
-MP_RVA_MIX = 1
-MP_RVA_ALBUM = 2
-MP_RVA_MAX = MP_RVA_ALBUM
-end
+  enum MP_Parms
+    MP_VERBOSE   = 0
+    MP_FLAGS
+    MP_ADD_FLAGS    MP_FORCE_RATE
+    MP_DOWN_SAMPLE    MP_RVA
+    MP_DOWNSPEED    MP_UPSPEED
+    MP_START_FRAME    MP_DECODE_FRAMES
+    MP_ICY_INTERVAL    MP_OUTSCALE
+    MP_TIMEOUT    MP_REMOVE_FLAGS
+    MP_RESYNC_LIMIT    MP_INDEX_SIZE
+    MP_PREFRAMES    MP_FEEDPOOL
+    MP_FEEDBUFFER
+  end
 
-enum MP_Errors
-MP_DONE = -12
-MP_NEW_FORMAT = -11
-MP_NEED_MORE = -10
-MP_ERR = -1
-MP_OK = 0
-MP_BAD_OUTFORMAT
-MP_BAD_CHANNEL
-MP_BAD_RATE
-MP_ERR_16TO8TABLE
-MP_BAD_PARAM
-MP_BAD_BUFFER
-MP_OUT_OF_MEM
-MP_NOT_INITIALIZED
-MP_BAD_DECODER
-MP_BAD_HANDLE
-MP_NO_BUFFERS
-MP_BAD_RVA
-MP_NO_GAPLESS
-MP_NO_SPACE
-MP_BAD_TYPES
-MP_BAD_BAND
-MP_ERR_NULL
-MP_ERR_READER
-MP_NO_SEEK_FROM_END
-MP_BAD_WHENCE
-MP_NO_TIMEOUT
-MP_BAD_FILE
-MP_NO_SEEK
-MP_NO_READER
-MP_BAD_PARS
-MP_BAD_INDEX_PAR
-MP_OUT_OF_SYNC
-MP_RESYNC_FAIL
-MP_NO_8BIT
-MP_BAD_ALIGN
-MP_NULL_BUFFER
-MP_NO_RELSEEK
-MP_NULL_POINTER
-MP_BAD_KEY
-MP_NO_INDEX
-MP_INDEX_FAIL
-MP_BAD_DECODER_SETUP
-MP_MISSING_FEATURE
-MP_BAD_VALUE
-MP_LSEEK_FAILED
-MP_BAD_CUSTOM_IO
-MP_LFS_OVERFLOW
-MP_INT_OVERFLOW
-end
+  enum MP_ParamFlags
+    MP_FORCE_MONO          =     0x7
+    MP_MONO_LEFT           =     0x1
+    MP_MONO_RIGHT          =     0x2
+    MP_MONO_MIX            =     0x4
+    MP_FORCE_STEREO        =     0x8
+    MP_FORCE_8BIT          =    0x10
+    MP_QUIET               =    0x20
+    MP_GAPLESS             =    0x40
+    MP_NO_RESYNC           =    0x80
+    MP_SEEKBUFFER          =   0x100
+    MP_FUZZY               =   0x200
+    MP_FORCE_FLOAT         =   0x400
+    MP_PLAIN_ID3TEXT       =   0x800
+    MP_IGNORE_STREAMLENGTH =  0x1000
+    MP_SKIP_ID3V2          =  0x2000
+    MP_IGNORE_INFOFRAME    =  0x4000
+    MP_AUTO_RESAMPLE       =  0x8000
+    MP_PICTURE             = 0x10000
+    MP_NO_PEEK_END         = 0x20000
+    MP_FORCE_SEEKABLE      = 0x40000
+  end
 
-enum MP_feature_set
-MP_FEATURE_ABI_UTF8OPEN = 0
-MP_FEATURE_OUTPUT_8BIT
-MP_FEATURE_OUTPUT_16BIT
-MP_FEATURE_OUTPUT_32BIT
-MP_FEATURE_INDEX
-MP_FEATURE_PARSE_ID3V2
-MP_FEATURE_DECODE_LAYER1
-MP_FEATURE_DECODE_LAYER2
-MP_FEATURE_DECODE_LAYER3
-MP_FEATURE_DECODE_ACCURATE
-MP_FEATURE_DECODE_DOWNSAMPLE
-MP_FEATURE_DECODE_NTOM
-MP_FEATURE_PARSE_ICY
-MP_FEATURE_TIMEOUT_READ
-MP_FEATURE_EQUALIZER
-end
+  enum MP_Param_RVA
+    MP_RVA_OFF   = 0
+    MP_RVA_MIX   = 1
+    MP_RVA_ALBUM = 2
+    MP_RVA_MAX   = MP_RVA_ALBUM
+  end
+
+  enum MP_Errors
+    MP_DONE              = -12
+    MP_NEW_FORMAT        = -11
+    MP_NEED_MORE         = -10
+    MP_ERR               =  -1
+    MP_OK                =   0
+    MP_BAD_OUTFORMAT
+    MP_BAD_CHANNEL
+    MP_BAD_RATE
+    MP_ERR_16TO8TABLE
+    MP_BAD_PARAM
+    MP_BAD_BUFFER
+    MP_OUT_OF_MEM
+    MP_NOT_INITIALIZED
+    MP_BAD_DECODER
+    MP_BAD_HANDLE
+    MP_NO_BUFFERS
+    MP_BAD_RVA
+    MP_NO_GAPLESS
+    MP_NO_SPACE
+    MP_BAD_TYPES
+    MP_BAD_BAND
+    MP_ERR_NULL
+    MP_ERR_READER
+    MP_NO_SEEK_FROM_END
+    MP_BAD_WHENCE
+    MP_NO_TIMEOUT
+    MP_BAD_FILE
+    MP_NO_SEEK
+    MP_NO_READER
+    MP_BAD_PARS
+    MP_BAD_INDEX_PAR
+    MP_OUT_OF_SYNC
+    MP_RESYNC_FAIL
+    MP_NO_8BIT
+    MP_BAD_ALIGN
+    MP_NULL_BUFFER
+    MP_NO_RELSEEK
+    MP_NULL_POINTER
+    MP_BAD_KEY
+    MP_NO_INDEX
+    MP_INDEX_FAIL
+    MP_BAD_DECODER_SETUP
+    MP_MISSING_FEATURE
+    MP_BAD_VALUE
+    MP_LSEEK_FAILED
+    MP_BAD_CUSTOM_IO
+    MP_LFS_OVERFLOW
+    MP_INT_OVERFLOW
+  end
+
+  enum MP_feature_set
+    MP_FEATURE_ABI_UTF8OPEN      = 0
+    MP_FEATURE_OUTPUT_8BIT
+    MP_FEATURE_OUTPUT_16BIT
+    MP_FEATURE_OUTPUT_32BIT
+    MP_FEATURE_INDEX
+    MP_FEATURE_PARSE_ID3V2
+    MP_FEATURE_DECODE_LAYER1
+    MP_FEATURE_DECODE_LAYER2
+    MP_FEATURE_DECODE_LAYER3
+    MP_FEATURE_DECODE_ACCURATE
+    MP_FEATURE_DECODE_DOWNSAMPLE
+    MP_FEATURE_DECODE_NTOM
+    MP_FEATURE_PARSE_ICY
+    MP_FEATURE_TIMEOUT_READ
+    MP_FEATURE_EQUALIZER
+  end
 end
 
 module Libmpg123
   class Mp
-
     @handle : Pointer(LibMP::MP_Handle) | Nil
     #  @error  : Int32
 
     property handle
+
     #  property error
 
     def initialize
@@ -193,12 +197,14 @@ module Libmpg123
       LibMP.feed(@handle, buf, size)
     end
 
-    #int mpg123_decode_frame(mpg123_handle *mh, off_t* num, unsigned char** audio, size_t* 	bytes)
+    def open_feed
+      LibMP.open_feed(@handle)
+    end
 
     def decode_frame(num, audio, bytes)
       LibMP.decode_frame(@handle, num, audio, bytes)
     end
-   
+
     def get_format(ratep, channelsp, encodingp)
       @error = LibMP.get_format(@handle, ratep, channelsp, encodingp)
     end
@@ -209,6 +215,14 @@ module Libmpg123
 
     def buf_size
       LibMP.buf_size(@handle)
+    end
+
+    def decode(in_buf, in_size, out_buf, out_size, done)
+      LibMP.decode(@handle, in_buf, in_size, out_buf, out_size, done)
+    end
+
+    def meta_free
+      LibMP.meta_free(@handle)
     end
 
     def exit
